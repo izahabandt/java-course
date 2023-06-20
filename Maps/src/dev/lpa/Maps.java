5d9d64ae2f29ef2bb1e977d0a813fd799a53b669
izahabandt <izahabandt@gmail.com>
2023-06-20 20:43:37 +0200

    maps-practice

--- Maps/src/dev/lpa/Maps.java
+++ Maps/src/dev/lpa/Maps.java
@@ -1,6 +1,7 @@
 package dev.lpa;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Maps {
@@ -12,7 +13,7 @@ public class Maps {
         employees.put(1111, "John");
         employees.put(2222, "George");
         employees.put(3333, "Kramer");
-        employees.put(4444, "Newman");
+        employees.put(4444, null);//you can have only one null as a key, multiple nulls as a value
 
 
         employees.put(4444, "Jerry"); //overrides Newman
@@ -20,9 +21,37 @@ public class Maps {
         // employees.putIfAbsent(1111, "Morty");
         // employees.remove(1111, "Morty");
 
-        System.out.println(employees);// hashMap doesn't guarantee order, doesn't have duplicate keys, but can duplicate values
+        //employees.keySet(); returns all keys
 
-        System.out.println(employees.containsKey(1111));
-        System.out.println(employees.containsValue("John"));
+        //  System.out.println(employees.containsKey(22222));
+
+        for(Integer employeeId : employees.keySet()){
+        System.out.println(employeeId);
+        }
+
+        for(Integer employeeId : employees.keySet()) {//to check what id Kramer has
+            if ("Kramer".equals(employees.get(employeeId))) {
+                System.out.println("Kramer's employee ID is " + employeeId);
+
+            }
+
+        Map<Integer, String> employees2 = new LinkedHashMap<>();// ordered map
+
+        employees2.put(1111, "John");
+        employees2.put(2222, "George");
+        employees2.put(3333, "Kramer");
+        employees2.put(4444, null);
+
+                System.out.println(employees2);
+            }
+
+        //    Map<Integer, String> employees2 = new TreeMap<>();// map ordered by the key
+        }
     }
-}
+
+        // System.out.println(employees);// hashMap doesn't guarantee order, doesn't have duplicate keys, but can duplicate values
+
+        //System.out.println(employees.containsKey(1111));
+        //System.out.println(employees.containsValue("John"));
+
+

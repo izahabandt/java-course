60855d81994ad5e3673243250a8d03563a7cb3f4
izahabandt <izahabandt@gmail.com>
2023-09-19 12:00:29 +0200

    db-practice

---
+++ Music1/src/com/izahabandt/Main.java
@@ -0,0 +1,36 @@
+package com.izahabandt;
+
+import com.izahabandt.model.Artist;
+import com.izahabandt.model.Datasource;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Datasource datasource = new Datasource();
+        if(!datasource.open()) {
+            System.out.println("Can't open datasource");
+            return;
+        }
+
+        List<Artist> artists = datasource.queryArtists(5);
+        if(artists == null) {
+            System.out.println("No artists!");
+            return;
+        }
+
+        for(Artist artist : artists) {
+            System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
+        }
+
+        List<String> albumsForArtist =
+                datasource.queryAlbumsForArtist("Pink Floyd", Datasource.ORDER_BY_ASC);
+
+        for(String album : albumsForArtist) {
+            System.out.println(album);
+        }
+
+        datasource.close();
+    }
+}
